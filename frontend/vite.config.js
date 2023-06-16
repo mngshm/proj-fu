ca1f160783fe13e478ec06e4d0161679e69e87c0
Mangeshrex <rex835138@gmail.com>
2023-06-16 17:12:50 +0530

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
-import { webserver_port } from '../sites/common_site_config.json'
+import { webserver_port } from '../../../sites/common_site_config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
